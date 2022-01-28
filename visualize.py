36cb932098a27eaef1394769a1666777537ebe8e
woojjn <76618935+woojjn@users.noreply.github.com>
2022-01-29 02:11:44 +0900

    modify visualize.py

--- visualize.py
+++ visualize.py
@@ -1,6 +1,10 @@
-from QBM import pickle_data_processing
+from QBM import PickleDataProcessing
 
 Ns = range(2, 7)
 ts = [1000, 2000, 4000, 8000, 16000, 32000]
+trial=20    
+result = PickleDataProcessing(4, ts, trials=(20, 30)).plot()
 
-pickle_data_processing(5, ts, trials=10).plot()
+
+# for i in range(trial):
+    
